23486ccbf76114f479c33c4c2570007e0d2a2c2a
Ramzi Chakroun <r.chakroun@root.sa>
2018-07-02 16:08:29 +0100

    Add project files.

---
+++ ListViewScrollToDemo/ListViewScrollToDemo/App.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListViewScrollToDemo
+{
+    public class App : Application
+    {
+
+        private MessageChatViewModel ViewModel
+        {
+            get { return BindingContext as MessageChatViewModel; }
+        }
+
+        public App()
+        {
+
+            BindingContext = new MessageChatViewModel();
+
+            var messageList = new ListView
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                ItemTemplate = new DataTemplate(typeof(MessageChatCell)),
+                HasUnevenRows = true,
+            };
+
+            var addMessageButton = new Button
+            {
+                Text = "Add message",
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.End,
+                Command = ViewModel.SendMessageButtonCommand,
+            };
+
+            MessagingCenter.Subscribe<MessageChatViewModel>(this, "ScrollToEnd", (sender) =>
+            {
+                var lastItem = messageList.ItemsSource.OfType<object>().Last();
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    if (lastItem != null)
+                    {
+                        messageList.ScrollTo(lastItem, ScrollToPosition.End, true);
+                    }
+                });
+            });
+
+            messageList.SetBinding(ListView.ItemsSourceProperty, new Binding("Messages"));
+
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.FillAndExpand,
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+
+                    Children =
+                    {
+                        messageList,
+                        addMessageButton
+                    }
+                }
+
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+
+
+    public class MessageChatViewModel
+    {
+        private long messageCounter = 0;
+
+        private ObservableCollection<MessageItem> messages;
+
+        public ObservableCollection<MessageItem> Messages
+        {
+            get
+            {
+                return messages;
+            }
+            set
+            {
+                messages = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private Command sendMessageButtonCommand;
+
+        public Command SendMessageButtonCommand
+        {
+            get
+            {
+                return sendMessageButtonCommand ?? (sendMessageButtonCommand = new Command(OnSendButtonCommand));
+            }
+        }
+
+        private void OnSendButtonCommand()
+        {
+            Messages.Add(new MessageItem
+            {
+                Text = "New message " + messageCounter,
+                SendDateString = DateTime.Now.ToString()
+            });
+
+            messageCounter++;
+
+            MessagingCenter.Send<MessageChatViewModel>(this, "ScrollToEnd");
+        }
+
+        public MessageChatViewModel()
+        {
+            Messages = new ObservableCollection<MessageItem>();
+        }
+
+
+    }
+
+
+    public class MessageItem
+    {
+        public string Text { get; set; }
+        public string SendDateString { get; set; }
+    }
+
+}
---
+++ ListViewScrollToDemo/ListViewScrollToDemo/MessageChatCell.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewScrollToDemo
+{
+    public class MessageChatCell : ViewCell
+    {
+
+        public MessageChatCell()
+        {
+            var messageTextLabel = new Label
+            {
+                HorizontalOptions = LayoutOptions.StartAndExpand,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+            };
+
+            var sendDateLabel = new Label
+            {
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                VerticalOptions = LayoutOptions.Center,
+                TextColor = Color.Black,
+            };
+
+            var messageGrid = new Grid();
+
+            messageGrid.BackgroundColor = Color.Gray;
+            messageGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(100, GridUnitType.Star) });
+            messageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(100, GridUnitType.Star) });
+            messageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(30, GridUnitType.Absolute) });
+
+            messageTextLabel.SetBinding(Label.TextProperty, new Binding("Text"));
+            sendDateLabel.SetBinding(Label.TextProperty, new Binding("SendDateString"));
+
+            messageGrid.Padding = new Thickness(10);
+            messageGrid.Children.Add(messageTextLabel, 0, 0);
+            messageGrid.Children.Add(sendDateLabel, 0, 1);
+
+            View = new ContentView
+            {
+                Content = messageGrid
+            };
+        }
+
+    }
+}
